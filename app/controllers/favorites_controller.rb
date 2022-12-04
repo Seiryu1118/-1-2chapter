f52dc0dfdf07b4593ccbe9266699564c3c443743
Seiryu1118 <116057980+Seiryu1118@users.noreply.github.com>
2022-12-04 06:30:56 +0000

    [Finish]#1-4

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,4 +1,4 @@
-class FavoriteController < ApplicationController
+class FavoritesController < ApplicationController
   def create
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.new(book_id: book.id)

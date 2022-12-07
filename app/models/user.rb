f9590beedf12f8904098245ab5bc71c5e384a1d7
Seiryu1118 <116057980+Seiryu1118@users.noreply.github.com>
2022-12-07 01:58:18 +0000

    [Finish]#1-5

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,11 +13,23 @@ class User < ApplicationRecord
   has_many :followers, through: :reverse_of_relationships, source: :follower
   has_one_attached :profile_image
 
-
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
 
-
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @user = User.where("name LIKE?", "#{word}")
+    elsif search == "forward_match"
+      @user = User.where("name LIKE?","#{word}%")
+    elsif search == "backward_match"
+      @user = User.where("name LIKE?","%#{word}")
+    elsif search == "partial_match"
+      @user = User.where("name LIKE?","%#{word}%")
+    else
+      @user = User.all
+    end
+  end
+ 
 
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
